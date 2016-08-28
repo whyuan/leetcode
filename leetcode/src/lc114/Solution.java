8e5aa1ae1f75f2cb6d6b22b1ad7af4212e764d76
519316166@qq.com <519316166@qq.com>
2016-08-28 23:45:45 +0800

    114

--- leetcode/src/lc114/Solution.java
+++ leetcode/src/lc114/Solution.java
@@ -1,5 +1,20 @@
 package lc114;
 
-public class Solution {
+import common.TreeNode;
 
+public class Solution {
+	private TreeNode preElement = null;
+    public void flatten(TreeNode root) {
+    	if (root == null) return;
+    	preElement = root;
+    	flatten(root.left);
+    	TreeNode leftLastElement = preElement;
+    	flatten(root.right);
+//    	TreeNode rightLastElement = preElement;
+    	if (leftLastElement != root) {
+			leftLastElement.right = root.right;
+			root.right = root.left;
+			root.left = null;
+    	}
+    }
 }
