72906c98b3b6e0ff10d9546eecac68b5dcb12f7f
whyuan <519316166@qq.com>
2016-08-31 21:21:19 +0800

    137

--- leetcode/src/lc137/Solution.java
+++ leetcode/src/lc137/Solution.java
@@ -5,15 +5,20 @@ import java.util.*;
 public class Solution {
     public int singleNumber(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        int a = 0;
+        int a = 0, b = 0, a0 = 0, b0 = 0;
         for (int i = 0; i < nums.length; i++) {
-        	a = (a^nums[i])&a;
+        	a0 = a;
+        	b0 = b;
+        	a = nums[i] ^ a;
+        	b = (nums[i] & a0) ^ b;
+        	a = a - (nums[i] & b0);
+        	b = b - (nums[i] & b0);
         }
         return a;
     }
     
     public static void main(String[] args) {
     	Solution solution = new Solution();
-    	System.out.println(solution.singleNumber(new int[]{1,1,1,2,3,3,3}));
+    	System.out.println(solution.singleNumber(new int[]{1,1,1,14,3,3,3}));
     }
 }
