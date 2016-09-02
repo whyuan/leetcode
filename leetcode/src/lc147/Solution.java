3f32a79cf16b972b1a735ee8597ceba652453a24
519316166@qq.com <519316166@qq.com>
2016-09-03 02:25:43 +0800

    146,147,148,149

---
+++ leetcode/src/lc147/Solution.java
@@ -0,0 +1,44 @@
+package lc147;
+
+import common.ListNode;
+import java.util.*;
+
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) return head;
+        ListNode cur = head.next;
+        ListNode preCur = head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        while (cur != null) {
+        	ListNode p = dummy;
+        	while (p.next.val < cur.val) {
+        		p = p.next;
+        	}
+        	if (p.next != cur) {
+				ListNode pNext = p.next;
+				ListNode curNext = cur.next;
+				p.next = cur;
+				cur.next = pNext;
+				preCur.next = curNext;
+				cur = curNext;
+        	} else {
+        		preCur = cur;
+        		cur = cur.next;
+        	}
+        }
+        return dummy.next;
+    }
+    
+    public static void main(String[] args) {
+    	Solution solution = new Solution();
+    	ListNode a = new ListNode(10);
+    	ListNode b = new ListNode(1);
+    	ListNode c = new ListNode(3);
+    	ListNode d = new ListNode(1);
+//    	a.next = b;
+//    	b.next = c;
+//    	c.next = d;
+    	solution.insertionSortList(a);
+    }
+}
---
+++ leetcode/src/lc149/Solution.java
@@ -0,0 +1,53 @@
+package lc149;
+
+import common.Point;
+import java.util.*;
+
+public class Solution {
+    public int maxPoints(Point[] points) {
+    	if (points == null || points.length == 0) return 0;
+    	Map<Integer, Map<Integer, Integer>> map = new HashMap<Integer, Map<Integer, Integer>>();
+    	int result = 0;
+    	for (int i = 0; i < points.length; i++) {
+    		map.clear();
+    		int overlap = 0;
+    		int max = 0;
+    		for (int j = i+1; j < points.length; j++) {
+    			int x = points[j].x-points[i].x;
+    			int y = points[j].y-points[i].y;
+    			if (x == 0 && y == 0) {
+    				overlap++;
+    				continue;
+    			}
+    			int gcdV = gcd(x, y);
+    			if (gcdV != 0) {
+    				x /= gcdV;
+    				y /= gcdV;
+    			}
+    			if (map.containsKey(x)) {
+    				if (map.get(x).containsKey(y)) {
+    					map.get(x).put(y, map.get(x).get(y)+1); 
+    				} else {
+    					map.get(x).put(y, 1);
+    				}
+    			} else {
+    				Map map0 = new HashMap<Integer, Integer>();
+    				map0.put(y, 1);
+    				map.put(x, map0);
+    			}
+    			max = Math.max(max, map.get(x).get(y));
+    		}
+    		result = Math.max(result, max+overlap);
+    	}
+    	return result;
+    }
+    
+    private int gcd(int a, int b) {
+    	if (b == 0) return a;
+    	else return gcd(b, a%b);
+    }
+    
+    public static void main(String[] args) {
+//    	solution.sortList(a);
+    }
+}
