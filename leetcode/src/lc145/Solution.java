a42102359e452ea5fac470ae5ff1ada3e2561ed1
whyuan <519316166@qq.com>
2016-09-05 09:07:53 +0800

    144,145,146,147,148,149

---
+++ leetcode/src/lc145/Solution.java
@@ -0,0 +1,66 @@
+package lc145;
+
+
+import common.TreeNode;
+import java.util.*;
+
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+//        return preorderTraversal(root, new ArrayList<Integer>());
+    	List<Integer> result = new ArrayList<Integer>();
+    	if (root == null) return result;
+    	ArrayList<TreeNode> q = new ArrayList<TreeNode>();
+    	ArrayList<Integer> state = new ArrayList<Integer>();
+    	q.add(root);
+    	state.add(0);
+    	int t = 1;
+    	while (t > 0) {
+    		TreeNode cur = q.get(t-1);
+    		Integer curState = state.get(t-1);
+    		// 减少调用次数
+    		if (curState == 0) {
+    			state.set(t-1, curState+1);
+    			t = this.deal(cur.left, t, q, state);
+    		} else if (curState == 1) {
+    			state.set(t-1, curState+1);
+    			t = this.deal(cur.right, t, q, state);
+    		} else {
+    			result.add(cur.val);
+    			t--;
+    		}
+    	}
+    	return result;
+    }
+    
+    private Integer deal(TreeNode node, Integer t, List<TreeNode> q, List<Integer> state) {
+		if (node != null) {
+			if (t == q.size()) {
+				q.add(node);
+				state.add(0);
+			} else {
+				q.set(t, node);
+				state.set(t, 0);
+			}
+			t++;
+		}
+		return t;
+    }
+    
+//    private List<Integer> preorderTraversal(TreeNode root, List<Integer> result) {
+//    	if (root == null) return result;
+//    	result.add(root.val);
+//    	preorderTraversal(root.left, result);
+//    	preorderTraversal(root.right, result);
+//    	return result;
+//    }
+    
+    public static void main(String[] args) {
+    	Solution solution = new Solution();
+    	TreeNode a = new TreeNode(1);
+    	TreeNode b = new TreeNode(2);
+    	TreeNode c = new TreeNode(3);
+    	a.left = b;
+    	a.right = c;
+    	solution.postorderTraversal(a);
+    }
+}
---
+++ leetcode/src/lc148/Solution.java
@@ -0,0 +1,21 @@
+package lc148;
+
+import java.util.*;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int a = 0, b = 0, a0 = 0;
+        for (int i = 0; i < nums.length; i++) {
+        	a0 = a;
+        	a = (nums[i] ^ a) ^ (nums[i] & b);
+        	b = ((a0 | b) & nums[i]) ^ b;
+        }
+        return a;
+    }
+    
+    public static void main(String[] args) {
+    	Solution solution = new Solution();
+    	System.out.println(solution.singleNumber(new int[]{0,1,0,1,0,1,98}));
+    }
+}
