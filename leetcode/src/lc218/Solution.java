8884e6028e2676be608ff99de9fa20f557aa5924
519316166@qq.com <519316166@qq.com>
2016-09-12 22:36:23 +0800

    update 221

--- leetcode/src/lc218/Solution.java
+++ leetcode/src/lc218/Solution.java
@@ -3,54 +3,56 @@ package lc218;
 import java.util.*;
 
 public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return 0;
-        return f(nums, k, 0, nums.length-1);
-    }
-    
-    public int f(int[] nums, int k, int start, int end) {
-        int h = start, t = end, temp = 0;
-        int xIndex = genP(nums, start, end);
-        temp = nums[xIndex];
-        nums[xIndex] = nums[start];
-        nums[start] = temp;
-        int x = nums[start];
-        while (h < t) {
-        	while (h < t && x > nums[t]) t--;
-        	while (h < t && x <= nums[h]) h++;
-        	if (h < t) {
-        		temp = nums[h];
-        		nums[h] = nums[t];
-        		nums[t] = temp;
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> result = new ArrayList<int[]>();
+        if (buildings == null || buildings.length == 0 || buildings[0].length == 0) return result;
+//        result.add(new int[]{buildings[0][0], buildings[0][2]});
+//        result.add(new int[]{buildings[0][1], 0});
+        int t = 0;
+        for (int i = 0; i < buildings.length; i++) {
+        	while (t+1 < result.size() && result.get(t+1)[0] < buildings[i][0]) t++;
+        	if (t == result.size()) {
+        		result.add(new int[]{buildings[i][0], buildings[i][2]});
+        		result.add(new int[]{buildings[i][1], 0});
+        	} else {
+        		if (buildings[i][2] >= result.get(t)[1]) {
+        			int t1 = t;
+        			while (t1+1 < result.size() && result.get(t1+1)[0] <= buildings[i][1]) t1++;
+        			int h = 0;
+        			if (t1 < result.size()) {
+        				h = result.get(t1)[1];
+        			}
+        			for (int i0 = t; i0 < t1; i0++) {
+        				result.remove(t+1);
+        			}
+        			result.add(t+1, new int[]{buildings[i][0], buildings[i][2]});
+        			result.add(t+2, new int[]{buildings[i][1], h});
+        		} else {
+        			int t2 = t;
+//        			while (t2+1 < result.size() && result.get(t2+1)[1] >= buildings[i][2] && result.get(t2+1)[0] <= buildings[i][1]) t2++;
+        			while (t2+1 < result.size() && result.get(t2+1)[1] >= buildings[i][2] && result.get(t2+1)[0] <= buildings[i][1]) t2++;
+        			if (result.get(t2)[0] <= buildings[i][1]) {
+        				int t3 = t2+1;
+        				while (t3+1 < result.size() && result.get(t3+1)[0] <= buildings[i][1]) t3++;
+        				int h = 0;
+        				if (t3 < result.size()) {
+        					h = result.get(t3)[1];
+        				}
+        				int x0 = result.get(t2)[0];
+        				for (int i1 = t2; i1 < t3; i1++) {
+        					result.remove(t2);
+        				}
+        				result.add(t2+1, new int[]{x0, buildings[i][2]});
+        				result.add(t2+2, new int[]{buildings[i][1], h});
+        			}
+        		}
         	}
         }
-        nums[start] = nums[h];
-        nums[h] = x;
-		if (h == k - 1) {
-			return nums[h];
-		} else if (h < k - 1) {
-			return f(nums, k, h + 1, end);
-		} else {
-			return f(nums, k, start, h - 1);
-		}
-    }
-
-    public int genP(int[] nums, int l, int r)
-    {
-        int m = (l+r)/2;
-        if (nums[l] > nums[r]) {
-        	if (nums[r]>nums[m]) return r;
-        	else return nums[l]>nums[m]?m:l;
-        } else {
-        	if (nums[l]>nums[m]) return l;
-        	else return nums[m]>nums[r]?r:m;
-        }
+        return result;
     }
 
     public static void main(String[] args) {
     	Solution solution = new Solution();
-    	System.out.println(solution.findKthLargest(new int[]{-1,2,0}, 3));
-    	System.out.println(solution.findKthLargest(new int[]{2,1}, 1));
-    	System.out.println(solution.findKthLargest(new int[]{4,4,4,4,4,4}, 6));
+//    	System.out.println(solution.findKthLargest(new int[]{-1,2,0}, 3));
     }
 }
