6206c7a674e0211a6e957cd48776b2e8a73a1f1a
whyuan <519316166@qq.com>
2016-09-17 00:03:41 +0800

    update

--- leetcode/src/lc283/Solution.java
+++ leetcode/src/lc283/Solution.java
@@ -3,10 +3,17 @@ package lc227;
 import java.util.*;
 
 public class Solution {
-    private int[] a = new int[]{1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,25,30};
-    public int nthUglyNumber(int n) {
-	if (n == 1) return 1;
-	
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) return;
+	int step = 0;
+	for (int i = 0; i < nums.length; i++) {
+	    if (nums[i] != 0) {
+                nums[step++] = nums[i];
+	    }
+	}
+	for (int i = step; i < nums.length; i++) {
+	    nums[i] = 0;
+	}
     }
 
     public static void main(String[] args) {
